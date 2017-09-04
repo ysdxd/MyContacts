c29907b971810bca8c719eb4644e4d85272d8ccc
云朽 <jiangshen.js@cainiao.com>
2017-09-04 19:31:49 +0800

    code review

--- MyContacts/main.m
+++ MyContacts/main.m
@@ -1,10 +1,3 @@
-//
-//  main.m
-//  私人通讯录
-//
-//  Created by piglikeyoung on 15/3/11.
-//  Copyright (c) 2015年 jinheng. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
